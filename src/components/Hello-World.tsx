f69e6d381cd15e944d3e6220251bc1fd8069fb43
Vasile Draguța <dragutavasile@gmail.com>
2025-12-19 16:35:37 +0200

    Initial commit

---
+++ src/components/Hello-World.tsx
@@ -0,0 +1,9 @@
+const HelloWorld = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <div className='text-3xl font-bold underline'>Draguta Dev</div>
+    </div>
+  );
+};
+
+export default HelloWorld;
