b88d9c2b97162e3ea5276d89ccb85ee8ff5868fe
Vasile Draguța <dragutavasile@gmail.com>
2025-12-24 22:54:50 +0200

    credits page

---
+++ src/data/credits.ts
@@ -0,0 +1,23 @@
+export interface Credit {
+  name: string;
+  url: string;
+}
+
+export const credits: Credit[] = [
+  {
+    name: 'Cristian Crețu',
+    url: 'https://cretu.dev',
+  },
+  {
+    name: 'Gavin Nelson',
+    url: 'https://nelson.co',
+  },
+  {
+    name: 'Ruru',
+    url: 'https://www.ruru.build',
+  },
+  {
+    name: 'Emil Kowalski',
+    url: 'https://emilkowal.ski/',
+  },
+];
